ed5de6a6d274835fabc7117d645279ad4f416ead
gaomd <1244026727@qq.com>
2019-05-26 15:30:50 +0800

    delete unuse code & add libs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+import 'normalize.css'
+import './assets/styles/border.css'
+
+fastClick.attach(document.body);
 
 Vue.config.productionTip = false
 
